070aa0768fccf6a7940e7e84f535867a865faeef
DroidFreak32 <rushabshah32@gmail.com>
2023-05-14 00:32:22 +0000

    mirror: Import rsync script

---
+++ scripts/nginx-cloudflare.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+set -e
+
+cf_ips() {
+echo "# https://www.cloudflare.com/ips"
+
+for type in v4 v6; do
+echo "# IP$type"
+curl -sL "https://www.cloudflare.com/ips-$type/" | sed "s|^|allow |g" | sed "s|\$|;|g"
+echo
+done
+
+echo "# Generated at $(LC_ALL=C date)"
+}
+
+(cf_ips && echo "deny all; # deny all remaining ips") > /etc/nginx/allow-cloudflare-only.conf
+
+# reload Nginx
+# sudo systemctl reload nginx
+
